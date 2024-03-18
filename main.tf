a34f4aa4bb75978d3a776d81610d8731e26acfbf
Léandre <leandre.chavanne@gmail.com>
2024-03-19 00:19:50 +0100

    add ignore, vpc, subnet, v1 security group

--- main.tf
+++ main.tf
@@ -7,9 +7,78 @@ terraform {
   }
 }
 
-# la région aws ou nous voulons déployer nos différentes ressources
 provider "aws" {
   region     = "eu-west-3"
   access_key = var.access_key
   secret_key = var.secret_key
+}
+
+## VPC
+resource "aws_vpc" "poukifactory_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "poukifactory-vpc"
+  }
+}
+
+## Subnet
+resource "aws_subnet" "poukifactory_subnet" {
+  vpc_id                  = aws_vpc.poukifactory_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-3a"
+  tags = {
+    Name = "poukifactory-subnet",
+  }
+  depends_on = [aws_vpc.poukifactory_vpc]
+}
+
+## Security Group
+resource "aws_security_group" "poukifactory_sg" {
+  name   = "poukifactory_sg"
+  vpc_id = aws_vpc.poukifactory_vpc.id
+
+  ingress {
+    from_port   = 15777
+    to_port     = 15777
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 15000
+    to_port     = 15000
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 7777
+    to_port     = 7777
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 15777
+    to_port     = 15777
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 15000
+    to_port     = 15000
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 7777
+    to_port     = 7777
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "poukifactory-sg"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "access_key" {
-  type     = string
-  nullable = false
+  type      = string
+  nullable  = false
   sensitive = true
 }
 
 variable "secret_key" {
-  type     = string
-  nullable = false
+  type      = string
+  nullable  = false
   sensitive = true
 }
