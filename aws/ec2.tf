704a7e670f159443293e77290af0dca07df0a317
Léandre <leandre.chavanne@gmail.com>
2024-03-26 17:44:47 +0100

    fmt

--- aws/ec2.tf
+++ aws/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
-  ami           = "ami-0ebd2bf0042bb3e85"
-  instance_type = "t2.medium"
-  key_name      = "poukifactory_keypair"
+  ami                    = "ami-0ebd2bf0042bb3e85"
+  instance_type          = "t2.medium"
+  key_name               = "poukifactory_keypair"
   user_data              = file("user_data.sh")
   subnet_id              = aws_subnet.poukifactory_subnet.id
   vpc_security_group_ids = [aws_security_group.poukifactory_sg.id]
